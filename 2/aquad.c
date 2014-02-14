2c7e81f1c02ca70f0ffd061838a0fdfa14490707
Jacob Essex <jacob@jacobessex.com>
2014-02-14 15:54:35 +0000

    Now allocates jobs, now passes less data

--- 2/aquad.c
+++ 2/aquad.c
@@ -71,45 +71,55 @@ int main(int argc, char** argv ) {
 double farmer(int numprocs) {
 
     double totalArea = 0;
-    bool* waitingTasks = calloc(numprocs, sizeof(bool));
+    int numWorkingTasks = 0;
+    bool* workingTasks = calloc(numprocs, sizeof(bool));
 
     stack* work_stack = new_stack();
 
     double initial[2] = { A, B };
     MPI_Send(initial ,2, MPI_DOUBLE, 1, TAG_WORK, MPI_COMM_WORLD);
+    workingTasks[1] = true;
+    ++numWorkingTasks;
 
     while ( true ){
         MPI_Status status;
-        int hasMessage = false;
-        MPI_Iprobe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &hasMessage, &status);
+        MPI_Probe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 
-        if ( hasMessage ){
-            if ( status.MPI_TAG == TAG_RESULT ){
-                double partialResult;
-                MPI_Recv(&partialResult, 1, MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-                totalArea += partialResult;
-            }else{
-                double data[3];
-                MPI_Recv(data, 3, MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+        if ( status.MPI_TAG == TAG_RESULT ){
+            double partialResult;
+            MPI_Recv(&partialResult, 1, MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            totalArea += partialResult;
+        }else{
+            double data[3];
+            MPI_Recv(data, 3, MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 
-                double lhs[2] = {data[0], data[1]};
-                double rhs[2] = {data[1], data[2]};
+            double lhs[2] = {data[0], data[1]};
+            double rhs[2] = {data[1], data[2]};
 
-                push(lhs, work_stack);
-                push(rhs, work_stack);
-            }
+            push(lhs, work_stack);
+            push(rhs, work_stack);
+        }
+        workingTasks[status.MPI_SOURCE] = false;
+        --numWorkingTasks;
 
-            if ( is_empty(work_stack) ) {
-                break;
-            }else{
-                double* work = pop(work_stack);
-                MPI_Send(work, 2, MPI_DOUBLE, status.MPI_SOURCE, TAG_WORK, MPI_COMM_WORLD);
+        if ( numWorkingTasks == 0 && is_empty(work_stack) ) {
+            break;
+        }else{
+            for ( int i = 1; i < numprocs; ++i ){
+                if ( !is_empty(work_stack) && !workingTasks[i] ){
+                    double* work = pop(work_stack);
+
+                    ++numWorkingTasks;
+                    ++tasks_per_process[i];
+                    workingTasks[status.MPI_SOURCE] = true;
+                    MPI_Send(work, 2, MPI_DOUBLE, i, TAG_WORK, MPI_COMM_WORLD);
+                }
             }
         }
     }
 
     for ( int i = 1; i < numprocs; ++i ){
-        MPI_Send(initial, 2, MPI_DOUBLE, i, TAG_HALT, MPI_COMM_WORLD);
+        MPI_Send(NULL, 0, MPI_DOUBLE, i, TAG_HALT, MPI_COMM_WORLD);
     }
 
     return totalArea;
@@ -121,6 +131,13 @@ void worker(int mypid) {
     bool first = true;
 
     while ( true ){
+        MPI_Probe(FARMER_ID, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+
+        if ( status.MPI_TAG == TAG_HALT ){
+            MPI_Recv(NULL, 0, MPI_DOUBLE, FARMER_ID, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            break;
+        }
+
         double data[2];
         MPI_Recv(data, //out intial address of buffer
                  2, //count
@@ -134,7 +151,7 @@ void worker(int mypid) {
             break;
         }
 
-        sleep(SLEEPTIME);
+        usleep(SLEEPTIME);
 
         double left = data[0];
         double right = data[1];
