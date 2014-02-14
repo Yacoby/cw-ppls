dbb82934a62362bc7253b15e53b3607d9fd42279
Jacob Essex <jacob@jacobessex.com>
2014-02-14 13:29:00 +0000

    Minor code corrections

--- 2/aquad.c
+++ 2/aquad.c
@@ -11,19 +11,21 @@
 
 #define SLEEPTIME 1
 
-int *tasks_per_process;
+#define FARMER_ID 0
+
+int* tasks_per_process;
 
 double farmer(int);
 
 void worker(int);
 
-int main(int argc, char **argv ) {
+int main(int argc, char** argv ) {
     int i, myid, numprocs;
     double area, a, b;
 
     MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
-    MPI_Comm_rank(MPI_COMM_WORLD,&myid);
+    MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myid);
 
     if(numprocs < 2) {
         fprintf(stderr, "ERROR: Must have at least 2 processes to run\n");
@@ -31,21 +33,17 @@ int main(int argc, char **argv ) {
         exit(1);
     }
 
-    if (myid == 0) { // Farmer
-        // init counters
-        tasks_per_process = (int *) malloc(sizeof(int)*(numprocs));
-        for (i=0; i<numprocs; i++) {
-            tasks_per_process[i]=0;
-        }
+    if (myid == FARMER_ID) {
+        tasks_per_process = (int*) cmalloc(numprocs, sizeof(int));
     }
 
-    if (myid == 0) { // Farmer
+    if (myid == FARMER_ID) {
         area = farmer(numprocs);
-    } else { //Workers
+    } else {
         worker(myid);
     }
 
-    if(myid == 0) {
+    if(myid == FARMER_ID) {
         fprintf(stdout, "Area=%lf\n", area);
         fprintf(stdout, "\nTasks Per Process\n");
         for (i=0; i<numprocs; i++) {
