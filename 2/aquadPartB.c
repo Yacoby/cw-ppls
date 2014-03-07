f60d608b9ee4bea77d23d9e96ce5095926f522a8
Jacob Essex <jacob@jacobessex.com>
2014-03-07 23:26:19 +0000

    Now use scatter gather

--- 2/aquadPartB.c
+++ 2/aquadPartB.c
@@ -2,10 +2,30 @@
  * This version is much simpler than Part A.
  *
  * For each worker the farmer sends a range to work on. The worker
- * then 
+ * then sends back the total area and the number of iterations of quad per process
  *
- * Using the fact that all tasks need to finish for the farmer to compute the
- * result there is no need to continously poll to see )
+ * Three primitives are used: MPI_Scatter, MPI_Gather and MPI_Reduce.
+ *
+ * The farmer array is built containing the work for each process
+ *
+ * MPI_Scatter is used to distribute work to all worker processes.
+ * Each worker then does the work assigned using the quad function
+ *
+ * MPI_Reduce is then used to sum everything into a single variable using the MPI_SUM
+ * operation and return the result to the farmer.
+ *
+ * MPI_Gather is used to gather up the number of calls to quad for each process
+ * and return them to the farmer.
+ *
+ * MPI_Scatter and MPI_Gather were used as they fitted the model. It would have also 
+ * better possible to use MPI_Send and MPI_Recv for each process but this would have
+ * just been doing the same thing as Scatter Gather but (probably) worse.
+ *
+ * MPI_Reduce was used to gather all the areas and sum them together. This was instead of
+ * sending the timing data back from the workers to the farmer using MPI_Gather. This seemed
+ * more elegant and avoided data processing on the worker letting MPI do the heavy lifting.
+ * The one possible downside was that it would involve more messages. Using just MPI_Gather the number
+ * of calls could be sent in an length two array with the area.
  */
 
 
@@ -78,22 +98,23 @@ int main(int argc, char** argv ) {
 }
 
 double farmer(int numprocs) {
+    double* data = (double*)calloc(numprocs*2, sizeof(double));
+    const int splits = numprocs - 1;
+    for ( int i = 1; i < numprocs; ++i ){
+        data[i*2] = (B-A)/splits*(i-1);
+        data[i*2+1] = (B-A)/splits*i;
+    }
+
+    MPI_Scatter(data, 2, MPI_DOUBLE, data, 2, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    free(data);
+
+    //with these functions the root process has to send, exploits the fact that the first items of
+    //data are always 0
     double totalArea = 0;
+    MPI_Reduce(data, &totalArea, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 
-    float splits = numprocs - 1;
-    for ( int i = 1; i < numprocs; ++i ){
-        double range[2] = { (B-A)/splits*(i-1), (B-A)/splits*i};
-        MPI_Send(range, 2, MPI_DOUBLE, i, TAG_WORK, MPI_COMM_WORLD);
-    }
-
-    for ( int i = 1; i < numprocs; ++i ){
-        double result[2];
-        MPI_Status status;
-        MPI_Recv(result, 2, MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-
-        totalArea += result[0];
-        tasks_per_process[i] = result[1];
-    }
+    //again exploiting the fact that tasks_per_process[0] = 0
+    MPI_Gather(tasks_per_process, 1, MPI_INT, tasks_per_process, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
     return totalArea;
 }
@@ -101,7 +122,7 @@ double farmer(int numprocs) {
 double quad(double left, double right, double fleft, double fright, double lrarea, int* timesCalled) {
     double mid, fmid, larea, rarea;
 
-    ++(*timesCalled);
+    ++*timesCalled;
 
     mid = (left + right) / 2;
     fmid = F(mid);
@@ -115,22 +136,16 @@ double quad(double left, double right, double fleft, double fright, double lrare
 }
 
 void worker(int mypid) {
-    MPI_Status status;
-
     double data[2];
-    MPI_Recv(data, //out intial address of buffer
-             2, //count
-             MPI_DOUBLE, //data type
-             FARMER_ID, //source
-             MPI_ANY_TAG, //tag
-             MPI_COMM_WORLD, //communication
-             &status); //status object
+
+    MPI_Scatter(NULL, 0, MPI_DOUBLE, data, 2, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
     double a = data[0];
     double b = data[1];
 
     int timesCalled = 0;
     double area = quad(a, b, F(a), F(b), (F(a)+F(b)) * (b-a)/2, &timesCalled);
-    double result[2] = {area, timesCalled};
-    MPI_Send(result, 2, MPI_DOUBLE, FARMER_ID, TAG_RESULT, MPI_COMM_WORLD);
+
+    MPI_Reduce(&area, NULL, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    MPI_Gather(&timesCalled, 1, MPI_INT, NULL, 0, MPI_INT, 0, MPI_COMM_WORLD);
 }
